e9a3fae064c6ffe7fb98f13dbbbc449b9d6dd2d9
adriancr1 <adrian.casal.rodriguez@gmail.com>
2023-12-07 18:52:02 +0100

    end 6 section

---
+++ README.md
@@ -0,0 +1,15 @@
+# Node-ts-multiplication-app
+
+Introductions to run and execute our program
+
+1. Install dep
+
+```
+npm install 
+```
+
+2. run
+
+```
+npm run dev
+```
---
+++ src/domain/use-cases/save-file.use-case.ts
@@ -0,0 +1,33 @@
+import fs from 'fs';
+
+export interface SaveFileUSeCase {
+    execute: ( options: Options ) => boolean;
+}
+
+export interface Options {
+    fileContent     : string;
+    fileDestination?: string;
+    fileName?       : string;
+}
+
+export class SaveFile implements SaveFileUSeCase{
+
+    constructor(
+        /** repository: storageRepository */
+    ){}
+
+    execute( {  
+        fileContent, 
+        fileDestination = 'outputs', 
+        fileName = 'table'
+    } : Options): boolean {
+        try {
+            fs.mkdirSync(fileDestination, { recursive: true });
+            fs.writeFileSync(`${ fileDestination }/${ fileName }.txt`, fileContent );
+            return true;                
+        } catch (error) {
+            console.error(error);
+            return false;
+        }
+    }
+}
